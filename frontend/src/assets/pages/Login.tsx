5d415d95c015c15144f0877523708e171ff6f30d
Julian Dworzycki <mr.jujek@gmail.com>
2024-01-31 20:32:59 +0100

    Adding manifest

---
+++ frontend/src/assets/pages/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Login() {
+
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+            <p>This is a React component.</p>
+        </div>
+    );
+}
+
+export default Login;
