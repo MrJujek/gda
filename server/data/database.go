a78f51ca2f247c9fd137bfd44075b3bbf4139895
Plasny <git.plasny.uq95y@slmail.me>
2024-02-01 18:56:20 +0100

    Adding database user creation and deletion
    Creation and deletion happens according to the list retrived from LDAP/AD server.

--- server/data/database.go
+++ server/data/database.go
@@ -10,6 +10,8 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// TODO cleanup
+
 var connStr string
 
 const (
@@ -22,6 +24,9 @@ Check if database is up and whether your configuration is correct.`
 	dbRollbackErr = `
 Unable to rollback changes, with error: %v
 Check if database is up and whether your configuration is correct.`
+	ldapErr = `
+Importing users from LDAP/AD failed.
+Check if LDAP/AD server is up and whether your configuration is correct.`
 )
 
 func getDbConn() (*sqlx.DB, error) {
@@ -59,6 +64,96 @@ func createDbSchema() {
 	log.Print("Database schema created.")
 }
 
+// This function exists on error
+func addUsersFromLDAP() {
+	type dn_id struct {
+		Id uint32 `db:"id"`
+		Dn string `db:"ldap_dn"`
+	}
+
+	var (
+		ids      []dn_id
+		newUsers []LdapUser
+		delUsers []dn_id
+	)
+
+	db, err := getDbConn()
+	if err != nil {
+		log.Fatal(dbConnErr)
+	}
+	defer db.Close()
+
+	err = db.Select(&ids, "SELECT id, ldap_dn FROM users WHERE deleted = false")
+	if err != nil {
+		log.Print("select")
+		log.Print(err)
+		log.Fatal(ldapErr)
+	}
+
+	ldapUsers, err := GetUserListLDAP()
+	if err != nil {
+		log.Fatal(ldapErr)
+	}
+
+outerAdd:
+	for _, ldU := range ldapUsers {
+		for _, dbU := range ids {
+			if dbU.Dn == ldU.Dn {
+				continue outerAdd
+			}
+		}
+
+		newUsers = append(newUsers, ldU)
+	}
+
+outerDel:
+	for _, dbU := range ids {
+		for _, ldU := range ldapUsers {
+			if dbU.Dn == ldU.Dn {
+				continue outerDel
+			}
+		}
+
+		delUsers = append(delUsers, dbU)
+	}
+
+	tx, err := db.Beginx()
+	if err != nil {
+		log.Fatal(ldapErr)
+	}
+
+	for _, user := range newUsers {
+		_, err := tx.Exec(
+			`INSERT INTO users (ldap_dn, common_name, display_name) 
+                VALUES ($1, $2, $3)`,
+			user.Dn, user.CommonName, user.DisplayName,
+		)
+		if err != nil {
+			if err := tx.Rollback(); err != nil {
+				log.Fatalf(dbRollbackErr, err)
+			}
+			log.Fatal(ldapErr)
+		}
+	}
+
+	for _, user := range delUsers {
+		_, err := tx.Exec("UPDATE users SET deleted = true WHERE id = $1", user.Id)
+		if err != nil {
+			if err := tx.Rollback(); err != nil {
+				log.Fatalf(dbRollbackErr, err)
+			}
+			log.Fatal(ldapErr)
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(ldapErr)
+	}
+
+	log.Print("Users imported from LDAP/AD.")
+}
+
 func InitDB() {
 	user := u.EnvExit("DB_USER")
 	pass := u.EnvExit("DB_PASS")
@@ -85,6 +180,8 @@ func InitDB() {
 	log.Print("Connection with database established.")
 
 	createDbSchema()
+
+	addUsersFromLDAP()
 
 	log.Print("Database is setup.")
 }
