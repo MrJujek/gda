b92cf07afa625fb121f1e1ca495e862889a0b9e0
Plasny <git.plasny.uq95y@slmail.me>
2024-02-19 22:41:28 +0100

    Updating database schema

--- server/db_wrapper/db_schema.go
+++ server/db_wrapper/db_schema.go
@@ -11,8 +11,8 @@ var db_schema = []string{
                 common_name     TEXT NOT NULL,
                 display_name    TEXT NOT NULL,
                 img             BYTEA,
-                active          BOOLEAN DEFAULT false NOT NULL,
-                deleted         BOOLEAN DEFAULT false NOT NULL,
+                active          BOOLEAN NOT NULL DEFAULT false,
+                deleted         BOOLEAN NOT NULL DEFAULT false,
                 public_key      TEXT,
                 pass_priv_key   TEXT,
                 code_priv_key   TEXT
@@ -20,12 +20,59 @@ var db_schema = []string{
         END IF;
         IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'sessions') THEN
             CREATE TABLE sessions(
-                session_uuid    UUID DEFAULT gen_random_uuid() UNIQUE PRIMARY KEY,
+                session_uuid    UUID NOT NULL DEFAULT gen_random_uuid() UNIQUE PRIMARY KEY,
                 user_id         INTEGER NOT NULL REFERENCES users (id),
                 created_at      TIMESTAMP NOT NULL DEFAULT now(),
                 expires_at      TIMESTAMP NOT NULL DEFAULT date_add(now(), '3 MONTH') -- do it in a better way?
             );
         END IF;
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'session_type') THEN
+            CREATE TYPE session_type AS ENUM ('normal', 'first_login', 'key_reencryption');
+        END IF;
     END $_$
+    `,
+    `
+    DO $$
+    BEGIN
+        -- adding types to sessions
+        IF NOT EXISTS (
+            SELECT 1 FROM information_schema.columns 
+            WHERE table_name = 'sessions' AND column_name = 'type'
+        ) THEN
+            ALTER TABLE sessions ADD COLUMN type session_type NOT NULL DEFAULT 'first_login';
+        END IF;
+
+        -- adding salt for PBKDF2 encryption
+        CREATE EXTENSION IF NOT EXISTS pgcrypto;
+        IF NOT EXISTS (
+            SELECT 1 FROM information_schema.columns 
+            WHERE table_name = 'users' AND column_name = 'salt'
+        ) THEN
+            ALTER TABLE users ADD COLUMN salt BYTEA NOT NULL DEFAULT gen_random_bytes(16);
+        END IF;
+
+        -- changing key columns in users to use BYTEA type
+        IF NOT EXISTS (
+            SELECT 1 FROM information_schema.columns 
+            WHERE table_name = 'users' AND column_name = 'public_key' AND data_type = 'bytea'
+        ) THEN
+            ALTER TABLE users DROP COLUMN IF EXISTS public_key;
+            ALTER TABLE users ADD COLUMN public_key BYTEA;
+        END IF;
+        IF NOT EXISTS (
+            SELECT 1 FROM information_schema.columns 
+            WHERE table_name = 'users' AND column_name = 'pass_priv_key' AND data_type = 'bytea'
+        ) THEN
+            ALTER TABLE users DROP COLUMN IF EXISTS pass_priv_key;
+            ALTER TABLE users ADD COLUMN pass_priv_key BYTEA;
+        END IF;
+        IF NOT EXISTS (
+            SELECT 1 FROM information_schema.columns 
+            WHERE table_name = 'users' AND column_name = 'code_priv_key' AND data_type = 'bytea'
+        ) THEN
+            ALTER TABLE users DROP COLUMN IF EXISTS code_priv_key;
+            ALTER TABLE users ADD COLUMN code_priv_key BYTEA;
+        END IF;
+    END $$
     `,
 }
--- server/db_wrapper/types.go
+++ server/db_wrapper/types.go
@@ -8,21 +8,32 @@ import (
 )
 
 type User struct {
-	ID                uint32         `db:"id"`
-	LDAP_DN           string         `db:"ldap_dn" json:"-"`
-	CommonName        string         `db:"common_name"`
-	DisplayName       sql.NullString `db:"display_name"`
-	Image             sql.NullByte   `db:"img"`
-	Active            bool           `db:"active"`
-	Deleted           bool           `db:"deleted" json:"-"`
-	PublicKey         sql.NullString `db:"public_key"`
-	PassEncPrivateKey sql.NullString `db:"pass_priv_key"`
-	CodeEncPrivateKey sql.NullString `db:"code_priv_key"`
+	ID                uint32           `db:"id"`
+	LDAP_DN           string           `db:"ldap_dn" json:"-"`
+	CommonName        string           `db:"common_name"`
+	DisplayName       sql.Null[string] `db:"display_name"`
+	Image             sql.Null[[]byte] `db:"img" json:"-"`
+	Active            bool             `db:"active"`
+	Deleted           bool             `db:"deleted" json:"-"`
+	Salt              []byte           `db:"salt" json:"-"`
+	PublicKey         sql.Null[[]byte] `db:"public_key"`
+	PassEncPrivateKey sql.Null[[]byte] `db:"pass_priv_key" json:"-"`
+	CodeEncPrivateKey sql.Null[[]byte] `db:"code_priv_key" json:"-"`
 }
 
+
+type SessionType string
+
+const (
+	SessionNormal     SessionType = "normal"
+	SessionFirstLogin SessionType = "first_login"
+	SessionKeyUpdate  SessionType = "key_reencryption"
+)
+
 type Session struct {
-	UUID      uuid.UUID `db:"session_uuid"`
-	UserId    uint32    `db:"user_id"`
-	CreatedAt time.Time `db:"created_at"`
-	ExpiresAt time.Time `db:"expires_at"`
+	UUID      uuid.UUID   `db:"session_uuid"`
+	UserId    uint32      `db:"user_id"`
+	Type      SessionType `db:"type"`
+	CreatedAt time.Time   `db:"created_at"`
+	ExpiresAt time.Time   `db:"expires_at"`
 }
