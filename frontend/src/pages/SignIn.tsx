aefe85953c4b516695f91b2aefea57d3f403f65d
Julian Dworzycki <mr.jujek@gmail.com>
2024-02-22 16:57:29 +0100

    Adding login

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,13 +1,36 @@
 import { useState, FormEvent } from 'react';
+import { useNavigate } from "react-router-dom";
 
 function SignIn() {
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
+    const [name, setName] = useState<string>('');
+    const [pass, setPass] = useState<string>('');
 
-    const handleSubmit = (event: FormEvent) => {
+    const navigate = useNavigate();
+
+    const handleSubmit = async (event: FormEvent) => {
         event.preventDefault();
         // Handle sign-in logic here
-        console.log(`Email: ${email}, Password ${password}`);
+        console.log(`Email: ${name}, Password ${pass}`);
+
+        const res = await fetch("/api/session", {
+            method: "POST",
+            body: JSON.stringify({
+                user: name,
+                pass: pass
+            })
+        })
+
+        if (!res.redirected) {
+            // statusEl.style.color = "red";
+            // statusEl.innerText = res.statusText;
+            console.error(res.statusText);
+        } else {
+            sessionStorage.setItem("password", pass)
+            // window.location.href = res.url;
+            navigate("/chat");
+        }
+
+        // console.log(res)
     };
 
     return (
@@ -17,12 +40,12 @@ function SignIn() {
                 <form onSubmit={handleSubmit}>
                     <label className="block mb-2">
                         Email:
-                        <input type="email" value={email} onChange={e => setEmail(e.currentTarget.value)} required
+                        <input type="text" value={name} onChange={(e) => setName(e.currentTarget.value)} required
                             className="mt-1 p-2 w-full border border-gray-300 rounded" />
                     </label>
                     <label className="block mb-4">
                         Password:
-                        <input type="password" value={password} onChange={e => setPassword(e.currentTarget.value)} required
+                        <input type="password" value={pass} onChange={(e) => setPass(e.currentTarget.value)} required
                             className="mt-1 p-2 w-full border border-gray-300 rounded" />
                     </label>
                     <button type="submit" className="w-full py-2 px-4 bg-blue-600 text-white rounded hover:bg-blue-700">Sign In</button>
