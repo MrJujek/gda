8b7bf2452cc3115b7c54b4b3057fa0d01a4762d6
Julian Dworzycki <mr.jujek@gmail.com>
2024-02-25 18:28:50 +0100

    Editing login page

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -50,23 +50,67 @@ function SignIn() {
     };
 
     return (
-        <div className="flex items-center justify-center h-screen bg-gray-200">
-            <div className="p-6 bg-white rounded shadow-md w-80">
-                <h2 className="text-2xl font-bold mb-5 text-gray-800">Logowanie</h2>
-                {error && <p className="text-red-500 mb-4">{error}</p>}
-                <form onSubmit={handleSubmit}>
-                    <label className="block mb-2">
-                        Nazwa użytkownika:
-                        <input type="text" value={name} onChange={handleNameChange} required
-                            className={`mt-1 p-2 w-full border border-gray-300 rounded ${error && 'border-red-500'}`} />
-                    </label>
-                    <label className="block mb-4">
-                        Hasło:
-                        <input type="password" value={pass} onChange={handlePassChange} required
-                            className={`mt-1 p-2 w-full border border-gray-300 rounded ${error && 'border-red-500'}`} />
-                    </label>
-                    <button type="submit" className="w-full py-2 px-4 bg-blue-600 text-white rounded hover:bg-blue-700">Sign In</button>
-                </form>
+        <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                    Zaloguj się do swojego konta
+                </h2>
+            </div>
+
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <form className="space-y-6" onSubmit={handleSubmit}>
+                        <div>
+                            <label
+                                htmlFor="email"
+                                className="block text-sm font-medium text-gray-700"
+                            >
+                                Nazwa użytkownika
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    id="name"
+                                    name="name"
+                                    required
+                                    value={name}
+                                    onChange={handleNameChange}
+                                    className={`block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm ${error && 'border-red-500'}`}
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label
+                                htmlFor="password"
+                                className="block text-sm font-medium text-gray-700"
+                            >
+                                Hasło
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    required
+                                    value={pass}
+                                    onChange={handlePassChange}
+                                    className={`block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm ${error && 'border-red-500'}`}
+                                />
+                            </div>
+                        </div>
+
+                        {error && <p className="text-red-500 mb-4">{error}</p>}
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            >
+                                Zaloguj się
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
