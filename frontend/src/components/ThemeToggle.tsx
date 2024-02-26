ac00430dff89755635ece0980816cba511240de4
Jakub Owoc <kubus.owoc@gmail.com>
2024-02-26 21:18:40 +0100

    Styling changes

--- frontend/src/components/ThemeToggle.tsx
+++ frontend/src/components/ThemeToggle.tsx
@@ -12,7 +12,7 @@ function ThemeToggle({setDarkMode}: {setDarkMode: (darkMode: boolean) => void}){
     <div className="flex justify-center items-center">
       <label className="cursor-pointer">
         <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} className="sr-only" />
-        <div className="shadow-card flex h-[46px] w-[46px] items-center justify-center rounded-full" style={{ backgroundColor: isChecked ? '#7a7a7a' : '#fffd8d' }}>
+        <div className="shadow-card flex h-[46px] w-[46px] items-center justify-center rounded-full" style={{ backgroundColor: isChecked ? '#7a7a7a' : '#ECBD18' }}>
           <span className={`flex h-9 w-9 items-center justify-center rounded-full`} style={{
             transition: 'all 0.3s ease',
             boxShadow: '0 0 0 2px white',
