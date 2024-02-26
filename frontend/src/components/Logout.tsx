dea49f008be97ed83855a91a5e5a177dda0265df
Jakub Owoc <kubus.owoc@gmail.com>
2024-02-26 16:51:10 +0100

    Styling Logout button

--- frontend/src/components/Logout.tsx
+++ frontend/src/components/Logout.tsx
@@ -4,7 +4,11 @@ function Logout() {
     const { logout } = useAuth();
 
     return (
-        <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600" onClick={logout}>
+        <button 
+            className="text-white bg-cornflower-blue border border-red-500 px-4 py-2 rounded-lg hover:bg-dark-cornflower-blue hover:border-red-600 focus:outline-none focus:ring-2 focus:ring-red-200 focus:ring-opacity-50 transition duration-300 ease-in-out"
+            onClick={logout}
+            style={{ backgroundColor: '#6495ed', borderColor: '#ffffff' }} // Inline styles for specific color codes
+        >
             <h1>Wyloguj</h1>
         </button>
     );
