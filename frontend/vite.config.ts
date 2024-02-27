095be4912ea4cf0e7ef087ede4a7fb299a02c6ce
Plasny <git.plasny.uq95y@slmail.me>
2024-02-27 20:19:36 +0100

    Adding https to frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,20 @@
+import { readFileSync } from "fs";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+let httpsConfig: {key: File, cert: File};
+const keyPath = process.env.GDA_KEY_PATH || './config/server.key'
+const certPath = process.env.GDA_CERT_PATH || './config/server.crt'
+
+if (process.env.GDA_SECURE_SERVER == 1) {
+	httpsConfig = {
+		key: readFileSync(keyPath),
+		cert: readFileSync(certPath)
+	}
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +22,8 @@ export default defineConfig({
 	server: {
 		port: 3000,
 		proxy: {
-			"/api": "http://localhost:80",
+			"/api": "http://localhost",
 		},
+		https: httpsConfig
 	},
 });
